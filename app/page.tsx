d80820bc651444442ec985a3b017ff368a3cb980
nico <513993979@qq.com>
2024-10-05 02:49:50 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,7 @@ export default function Home() {
 
 
       <div>
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        发布页
       </div>
 
       <div className="grid mb-32 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-2 lg:text-left">
